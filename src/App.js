45093bb1925679177c17adcd0960fa180c6d3d43
kgw <kgw1993.06>
2019-05-11 11:36:42 +0900

    라우팅 적용

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React,{Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import HomePage from './page/home/HomePage'
+import LoginPage from './page/login/LoginPage'
+import SignupPage from './page/signup/SignupPage'
+import Header from './component/header/Header'
 
 
-function App() {
-  return (
-    <div>
-      
-    </div>
-  );
+/** 라우팅
+ * /
+ * /login
+ * /signup
+ * 
+ * */
+
+
+
+class App extends Component {
+
+  render(){
+
+      return(
+        <div>
+          
+          <Router>
+            <Header/>  
+            <Route path="/" exact component={HomePage}></Route>
+            <Route path="/login" component={LoginPage}></Route>
+            <Route path="/signup" component={SignupPage}></Route>
+          </Router>
+          
+        </div>
+      )
+
+  }
 }
+
+
+
 
 export default App;
