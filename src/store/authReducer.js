54e86f42d24e98c48c778bb9f4f68640c99fce81
kgw <kgw1993.06>
2019-05-11 11:50:32 +0900

    authReducer 추가

---
+++ src/store/authReducer.js
@@ -0,0 +1,13 @@
+
+const initailState = {
+    user: null,
+
+}
+export default function authReducer(state = initailState, action) {
+    switch (action.type) {
+
+
+        default:
+            return state;
+    }
+}
