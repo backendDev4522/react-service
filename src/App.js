6cbe23fcede13d974c50672a1b300c1a205d1c20
kgw <kgw1993.06>
2019-05-11 12:00:47 +0900

    Login 여부 확인
    로그인 안 했을 때 로그인 페이지로 리다이렉트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React,{Component} from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
 import HomePage from './page/home/HomePage'
 import LoginPage from './page/login/LoginPage'
 import SignupPage from './page/signup/SignupPage'
 import Header from './component/header/Header'
+import {connect} from 'react-redux'
 
 
 /** 라우팅
@@ -20,22 +21,29 @@ class App extends Component {
   render(){
 
       return(
-        <div>
-          
           <Router>
             <Header/>  
-            <Route path="/" exact component={HomePage}></Route>
+            <Route path="/" exact component={()=>{
+              if(this.props.user){
+                return <HomePage/>
+              } else {
+                return <Redirect to="/login"/>
+              }
+              
+            }} />
             <Route path="/login" component={LoginPage}></Route>
             <Route path="/signup" component={SignupPage}></Route>
           </Router>
-          
-        </div>
       )
 
   }
 }
 
+const mapStateToProps = (state) =>{
+    return {
+      user:state.auth.user
+    }
+}
 
 
-
-export default App;
+export default connect(mapStateToProps, null)(App);
