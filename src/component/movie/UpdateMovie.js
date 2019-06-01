ad71946c8d4881f371fc94036b3a2c091001b5b6
kgw <kgw1993.06>
2019-06-01 11:28:00 +0900

    업데이트 초기 작업

--- src/component/movie/UpdateMovie.js
+++ src/component/movie/UpdateMovie.js
@@ -1,41 +1,52 @@
 import React, { Component } from 'react'
-import { Grid, Form, Message} from 'semantic-ui-react'
+import { Grid, Form, Message, Loader} from 'semantic-ui-react'
 import { connect } from 'react-redux'
 import MovieForm from './MovieForm'
 class UpdateMovie extends Component {
 
+    componentDidMount(){
+        //id에 해당하는 영화 불러오기
+        console.log(this.props.movieId)
+    }
     
     render() {
-        const { error, isLoading } = this.props;
+        const { error, isLoading, doc } = this.props;
 
-    
-        return (
-            <Form>
-                <Grid>
+        if(doc){
+            const {name, imageURL, description, director} = doc.date();
+            return (
+                <Form>
                     <MovieForm ref="form"/>
-                    <Grid.Row centered>
-                        <Grid.Column mobile={16} tablet={8} computer={8}>
-                            {
-                                error ? <Message content={error.message} /> : null
-                            }
-                        </Grid.Column>
-                    </Grid.Row>
-                    <Grid.Row centered>
-                        <Grid.Column mobile={16} tablet={8} computer={8}>
-                            <Form.Button fluid loading={isLoading} onClick={this.onAddMovie}>수정 완료</Form.Button>
-                            <Form.Button fluid loading={isLoading} onClick={this.onAddMovie}>돌아 가기</Form.Button>
-                        </Grid.Column>
-                    </Grid.Row>
+                    <Grid>
+                        <Grid.Row centered>
+                            <Grid.Column mobile={16} tablet={8} computer={8}>
+                                {
+                                    error ? <Message content={error.message} /> : null
+                                }
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row centered>
+                            <Grid.Column mobile={16} tablet={8} computer={8}>
+                                <Form.Button fluid loading={isLoading} onClick={this.onAddMovie}>수정 완료</Form.Button>
+                                <Form.Button fluid loading={isLoading} onClick={this.onAddMovie}>돌아 가기</Form.Button>
+                            </Grid.Column>
+                        </Grid.Row>
+    
+                    </Grid>
+                </Form>
+            )
+        } else { 
+            return <Loader active style={{margin:16}}/>
 
-                </Grid>
-            </Form>
-        )
+        }
+      
     }
 }
 
 const mapStateToProps = (state) => {
     return {
-        
+        doc: state.updateMovie.doc,
+
 
     }
 }
